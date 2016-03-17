fe84f227375e9d16c7c9d0b0fd43c28c5ff1edd7
qinxiaochord <chordqinxiao@outlook.com>
2016-03-17 23:40:56 +0800

    add the homework4

---
+++ homework4/1.py
@@ -0,0 +1,22 @@
+# This program caculates the velocity of a freely falling object near 
+# Earth's surface employing the Euler method.
+
+from pylab import *
+
+v=[] #establish list v
+t=[] #establish list t
+g=9.8
+t0=0  #initialize t0
+t_end=float(raw_input('please input the end time:'))  #input the end time
+dt=float(raw_input('please input the time interval:'))   #input the time interval
+v0=float(raw_input('please input the initial velocity:'))  #input the initial velocity v0
+t.append(t0)
+v.append(v0)
+for i in range(int(t_end/dt)):
+    v.append(v[i]+g*dt)
+    i=i+1
+    t.append(i*dt)
+print t
+print v
+plot(t, v)
+show()
---
+++ homework4/4.py
@@ -0,0 +1,28 @@
+#This program solves two coupled ordinary differential equitions
+#corresponding to the radiative decay problem involving two types of nuclei.
+
+from pylab import *
+
+NA=[]   #list of numbers of A nucleus
+NB=[]   #list of numbers of B nucleus
+t=[]    #list of time
+NA0=float(raw_input('please input the inicial number of A :'))
+NB0=float(raw_input('please input the inicial number of B :'))
+tauA=float(raw_input('please input the decay time constant of A :'))
+tauB=float(raw_input('please input the decay time constant of B :'))
+t_end=float(raw_input('please input how long are the nucleus last :'))
+dt=float(raw_input('please input the time interval :'))
+NA.append(NA0)
+NB.append(NB0)
+t0=0    #inicialize t0
+t.append(t0)
+for i in range(int(t_end/dt)):
+    NA.append(NA[i]-NA[i]/tauA)
+    NB.append(NB[i]+NA[i]/tauA-NB[i]/tauB)
+    i=i+1
+    t.append(i*dt)
+print t
+print NA
+print NB
+#plot(t,NA,NB)
+#show()
