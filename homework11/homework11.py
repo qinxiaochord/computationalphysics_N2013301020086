b46be1b0eed7a325b0a11516e88d10c64b02505f
qinxiaochord <chordqinxiao@outlook.com>
2016-05-13 22:02:39 +0800

    adjust homework11_1

--- homework11/homework11.py
+++ homework11/homework11.py
@@ -10,31 +10,37 @@ from visual import *
 end_t = 10000.
 dt = 1.
 t = 0.
-Msun = 100
-Mearth = 100
-x20 = 100
-vx20 = 0
-vy20 = 1.
+m1 = 100.
+m2 = 200.
+r0 = 100.
+x10 = m2 * r0 / (m1 + m2)
+x20 = - m1 * r0 / (m1 + m2)
+#circle trajectory
+#p0 = math.sqrt(m1 * m1 * m2 * m2 / (r0 * (m1 + m2)))
+#ellipse trajectory. remark: p0 <= 2 * mu * m1 * m2 / r0, where mu=m1 * m2 / (m1 + m2) is the reduced mass of the system.
+p0 = 90.
+vy10 = p0 / m1
+vy20 = - p0 / m2
 
-earth = sphere(pos = (x20, 0, 0), radius = 5, color = color.white, material = materials.earth)
-sun = sphere(pos = (0, 0, 0), radius = 10, color = color.yellow, material = materials.emissive)
+body1 = sphere(pos = (x10, 0, 0), radius = 5, color = color.white, material = materials.earth)
+body2 = sphere(pos = (x20, 0, 0), radius = 10, color = color.yellow, material = materials.emissive)
 
-earth.velocity = vector(vx20, vy20, 0)
-sun.velocity = vector(0, 0, 0)
+body1.velocity = vector(0, vy10, 0)
+body2.velocity = vector(0, vy20, 0)
 
-earth.trail = curve(color = earth.color)
-sun.trail = curve(color = sun.color)
+body1.trail = curve(color = body1.color)
+body2.trail = curve(color = body2.color)
 
-earth.trail.append(pos = earth.pos)
-sun.trail.append(pos = sun.pos)
+body1.trail.append(pos = body1.pos)
+body2.trail.append(pos = body2.pos)
 
 while t < end_t:
     rate(100)
-    r = math.sqrt((earth.pos.x-sun.pos.x) ** 2 + (earth.pos.y-sun.pos.y) ** 2)
-    earth.velocity += Msun * (sun.pos - earth.pos) * dt / r ** 3
-    sun.velocity += Mearth * (earth.pos - sun.pos) * dt / r ** 3
-    earth.pos += earth.velocity * dt
-    sun.pos += sun.velocity * dt
-    earth.trail.append(pos = earth.pos)
-    sun.trail.append(pos = sun.pos)
+    r = math.sqrt((body1.pos.x-body2.pos.x) ** 2 + (body1.pos.y-body2.pos.y) ** 2)
+    body1.velocity += m2 * (body2.pos - body1.pos) * dt / r ** 3
+    body2.velocity += m1 * (body1.pos - body2.pos) * dt / r ** 3
+    body1.pos += body1.velocity * dt
+    body2.pos += body2.velocity * dt
+    body1.trail.append(pos = body1.pos)
+    body2.trail.append(pos = body2.pos)
     t += dt
